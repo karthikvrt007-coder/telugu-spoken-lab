74ffed6092521ae7ae52d1441b1dd7b4b5f5359e
karthikvrt007-coder <karthikvrt007@gmail.com>
2026-09-13 09:26:01 +0530

    Make offline phone app (PWA) so lessons run without a computer

---
+++ public/sw.js
@@ -0,0 +1,34 @@
+const CACHE = "tsl-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./data.js",
+  "./manifest.json",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((hit) => hit || fetch(e.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy));
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
